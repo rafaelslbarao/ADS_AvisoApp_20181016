e1c9f46aaa53bf99fe28b183794a7fbd3db4dfa5
Rafael Sarreta Lucindo <rafael@datamob.com.br>
2018-10-18 21:37:23 -0300

    Parcial

--- app/src/main/java/br/com/datamob/ads_avisoapp/webservice/TokenSquidex.java
+++ app/src/main/java/br/com/datamob/ads_avisoapp/webservice/TokenSquidex.java
@@ -1,5 +1,29 @@
 package br.com.datamob.ads_avisoapp.webservice;
 
+import android.content.Context;
+
+import com.android.volley.toolbox.StringRequest;
+
 public class TokenSquidex
 {
+    private static final String url
+            = "https://cloud.squidex.io/identity-server/connect/token";
+
+    private static final String grant_type
+            = "client_credentials";
+
+    private static final String client_id
+            = "avisosapp:androidapp";
+
+    private static final String client_secret
+            = "5ixsLdHiEiKpFAHxm4Z3FCKhufe/0L7bAXQBrg0XXjY=";
+
+    private static final String scope
+            = "squidex-api";
+
+
+    public static void getToken(Context context)
+    {
+        //StringRequest
+    }
 }
